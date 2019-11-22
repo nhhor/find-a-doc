00cb15e2470dd1241c2334fba9cb01e2222c3562
Noah Horwitz <00noah@gmail.com>
2019-11-22 11:34:30 -0800

    add search api by doctor name function, front js and html.

--- src/betterdoctor-service.js
+++ src/betterdoctor-service.js
@@ -8,4 +8,20 @@ export class BetterDoctorService {
       // console.error("There was an error handling your request: " + error.message);
     }
   }
+
+  async findDoctorByDoctorName(doctorName) {
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=45.523%2C-122.676%2C10&user_location=45.523%2C-122.676&sort=distance-asc&limit=10&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch(error) {
+      // console.error("There was an error handling your request: " + error.message);
+    }
+  }
+
+
+
+
+
+
 }
--- src/index.js
+++ src/index.js
@@ -4,30 +4,66 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { BetterDoctorService } from './../src/BetterDoctor-service.js';
 
-
-
-
 $(document).ready(function() {
 
-  let medicalIssue = "ear";
+  $('#medicalIssueButton').click(function() {
+    $("#medicalIssueResultsList").children().remove();
+    let medicalIssue = $("#medicalIssue").val();
 
-  (async () => {
-    let betterDoctorService = new BetterDoctorService();
-    let response = await betterDoctorService.findDoctorByMedicalIssue(medicalIssue);
-    getElements(response);
-  })();
+    (async () => {
+      let betterDoctorService = new BetterDoctorService();
+      let response = await betterDoctorService.findDoctorByMedicalIssue(medicalIssue);
+      getElements(response);
+    })();
 
-  function getElements(response) {
-    let keys = Object.keys(response.data);
-    // let values = Object.values(response.data[0]);
-    // console.log("response.data: ",response.data);
-    for (let i=0; i < keys.length; i++){
-      let docFirstName = response.data[i].profile.first_name;
-      let docLastName = response.data[i].profile.last_name;
-      let docTitle = response.data[i].profile.title;
-      let practiceName = response.data[i].practices[0].name;
-      $("#medicalIssueResultsList").append('<li>'+ docFirstName + ' ' + docLastName + ', ' + docTitle + '.<ul><li>Practicing at: ' + practiceName + '</li></ul>');
-      // console.log(docFirstName + ' ' + docLastName + ', ' + docTitle + '. Practicing at: ' + practiceName);
+    function getElements(response) {
+      let keys = Object.keys(response.data);
+      // let values = Object.values(response.data[0]);
+      // console.log("response.data: ",response.data);
+      for (let i=0; i < keys.length; i++){
+        let docFirstName = response.data[i].profile.first_name;
+        let docLastName = response.data[i].profile.last_name;
+        let docTitle = response.data[i].profile.title;
+        let practiceName = response.data[i].practices[0].name;
+        $("#medicalIssueResultsList").append('<li class="doctorNameClass">'+ docFirstName + ' ' + docLastName + ', ' + docTitle + '.<ul><li class="practiceClass">Practicing at: ' + practiceName + '</li></ul>');
+      }
     }
-  }
+  });
+
+
+  $('#doctorNameButton').click(function() {
+    $("#doctorNameResultsList").children().remove();
+    let doctorName = $("#doctorName").val();
+
+    (async () => {
+      let betterDoctorService = new BetterDoctorService();
+      let response = await betterDoctorService.findDoctorByDoctorName(doctorName);
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      let keys = Object.keys(response.data);
+      // let values = Object.values(response.data[0]);
+      // console.log("response.data: ",response.data);
+      for (let i=0; i < keys.length; i++){
+        let docFirstName = response.data[i].profile.first_name;
+        let docLastName = response.data[i].profile.last_name;
+        let docTitle = response.data[i].profile.title;
+        let practiceName = response.data[i].practices[0].name;
+        $("#doctorNameResultsList").append('<li class="doctorNameClass">'+ docFirstName + ' ' + docLastName + ', ' + docTitle + '.<ul><li class="practiceClass">Practicing at: ' + practiceName + '</li></ul>');
+      }
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
 });
